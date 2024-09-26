6c6aa7b3d64f7423b366006aa28fbf24f3c3509d
Saichandana-123 <147409798+Saichandana-123@users.noreply.github.com>
2024-09-27 01:26:46 +0530

    Update rabin_karp_search.cpp

--- src/rabin_karp_search.cpp
+++ src/rabin_karp_search.cpp
@@ -1,10 +1,35 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
+void search(char pat[],char txt[],int prime){
+    int M= strlen(pat);
+    int N= strlen(txt);
+    int i,j;
+    int h=1;
+    int p=0;//hash value of pattern
+    int t=0;//hash value of text 
+    int d=256;
+    int prime=INT_MAX;
+    //
+    for(i=0;i<M-1;i++)
+    h=(h*d)%prime;
 
-using namespace std;
+    for(i=0;i<M;i++){
+        p=(p*d+pat[i])%prime;
+        t=(t*d+txt[i])%prime;
+    }
 
-void RabinKarpSearch(const string& t, const string& p){
-    
+    for(i=0;i<N-M;i++){
+     if(p==t){
+        for( j=0;j<M;j++){
+              if(txt[i+j]!=pat[j]){
+                break;
+              }
+        }
+        if(j==M){
+            cout<<"pattern match at"<<i<<endl;
+        }
+     }
+     if(i<N-M){
+        t=(((t-txt[i]*h)*d)+txt[i+M])%prime;
+  }
+ 
+    }
 }
