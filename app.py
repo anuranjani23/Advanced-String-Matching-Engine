ad5e6ca6d540cb1c803f3cfab879372886bc8f86
anuranjani23 <anuranjani6174@gmail.com>
2024-09-27 03:15:06 +0530

    Initial commit

--- app.py
+++ app.py
@@ -3,3 +3,4 @@ from flask import Flask, render_template, request, redirect, flash
 from werkzeug.utils import secure_filename
 import subprocess
 import time
+
---
+++ build/aho_corasick_search.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
